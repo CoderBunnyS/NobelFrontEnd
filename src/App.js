90447fde822d550e3c881b3bdefe6d9cacd6280a
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2021-01-21 13:56:43 -0800

    styling tweaks

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Axios from "axios";
 import Card from "./components/Card";
 //import { CircularProgress } from "@material-ui/core/";
 import Header from "./components/Header";
+import Para from "./components/para";
 //import YButton from "./components/YButton"
 //import DButton from "./components/DButton"
 import Form from "./components/Create";
@@ -38,39 +39,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <img
-          className="img-responsive"
-          src="https://scx2.b-cdn.net/gfx/news/hires/2015/561381b103db1.jpeg"
-          alt="logo"
-        />
-        <div class="aboutPara">
-          <p>
-            When the Swedish businessman Alfred Nobel passed away in 1896, he
-            left behind what was then one of the world&rsquo;s largest private
-            fortunes. In his last will Nobel declared that the whole of his
-            remaining fortune of 31, 5 million Swedish crowns was to be invested
-            in safe securities and should constitute a fund{" "}
-            <em>
-              "the interest on which shall be annually distributed in the form
-              of prizes to those who, during the preceding year, shall have
-              conferred the greatest benefit on mankind"
-            </em>
-          </p>
-          <h3>Will</h3>
-          <p>
-            The will specified in which fields the prizes should be awarded
-            &ndash; physics, chemistry, medicine or physiology, literature and
-            peace &ndash; and which criteria the respective prize committees
-            should apply when choosing their prize recipients. According to the
-            will the Nobel Peace Prize was to be awarded{" "}
-            <em>
-              &ldquo;to the person who shall have done the most or the best work
-              for fraternity between the nations and the abolition or reduction
-              of standing armies and the formation and spreading of peace
-              congresses.&rdquo;
-            </em>
-          </p>
-        </div>
+        <Para />
 
         <Form />
 
